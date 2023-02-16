c76c427459fd684804e7f1ab56081d3f4516e54e
Nello Brunelli <nello.brunelli@wyscout.com>
2023-02-16 20:20:59 +0100

    First scaffolding of the project done

---
+++ src/components/Bottone.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Bottone extends Component {
+
+  render() {
+    return (
+      <div>
+        <button>
+          Ciao sono un bottone
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Bottone;
