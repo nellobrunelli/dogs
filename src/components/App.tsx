463ccaa127d51848f6739205befcce33cfb4df2d
Nello Brunelli <nello.brunelli@wyscout.com>
2023-03-05 16:23:49 +0100

    trying to fix the reducer

---
+++ src/components/App.tsx
@@ -0,0 +1,38 @@
+import { useReducer } from 'react'
+
+const ADD_TODO = 'addTodo'
+const UPDATE_VALUE = 'updateValue'
+
+type State = {
+  dogs:[]
+}
+
+const initialState: State = {
+  dogs: []
+}
+
+type Action = {
+  type: string, 
+  payload: string
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case ADD_TODO:
+      return state
+    case UPDATE_VALUE:
+      return state
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div> ci sono</div>
+  )
+}
+
+export default App
