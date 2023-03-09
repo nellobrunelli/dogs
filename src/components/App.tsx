1ef124fe2adf62eec08ab085772ecf2802bc16da
Nello Brunelli <nello.brunelli@wyscout.com>
2023-03-09 20:13:02 +0100

    error component done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,21 +3,28 @@ import React, { useReducer } from 'react'
 import {reducerDogs, StateDogsInit} from '../reducers/reducerDogs';
 import {reducerErrors, StateErrorsInit} from '../reducers/reducerErrors';
 import Dogs from './Dogs';
+import Error from './Error';
 
 function App() {
 
   const [stateDogs, dispatchDogs] = useReducer(reducerDogs, StateDogsInit)
   const [stateErrors, dispatchErrors] = useReducer(reducerErrors, StateErrorsInit)
 
-  return (
-    <div className="bg-">
+  const displayDom = () => {
+    return stateErrors.isActive ? (
+      <Error text={stateErrors.error} />
+    ) : (
       <Dogs 
         dogs={stateDogs} 
         errors={stateErrors}
         dispatchDogs={dispatchDogs} 
         dispatchErrors={dispatchErrors} 
         />
-    </div>
+      )
+    }
+
+  return (
+    <div className="">{displayDom()}</div>
   )
 }
 
