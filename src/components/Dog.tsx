be8057f5619e2709bfcd28e1100287ed3747adf2
Nello Brunelli <nello.brunelli@wyscout.com>
2023-03-10 20:43:28 +0100

    close button style

--- src/components/Dog.tsx
+++ src/components/Dog.tsx
@@ -16,9 +16,9 @@ const Dog:React.FC<Props> = ({url, dispatchDogs, mkey}) => {
         className="buttonDeleteDog"
         onClick={() => {dispatchDogs({type: 'DELETE_DOG', payload:url})}}
       >
-      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-      </svg>
+        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
       </button>
       <img src={url} />
     </div>
