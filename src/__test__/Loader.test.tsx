86eda19e5a3ff69afe7265371398040b09c8e3db
Nello Brunelli <nello.brunelli@wyscout.com>
2023-04-01 12:54:12 +0200

    test folder

--- src/__test__/Loader.test.tsx
+++ src/__test__/Loader.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import '@testing-library/jest-dom'
 import {render, screen} from '@testing-library/react'
 
-import Loader from './Loader'
+import Loader from '../components/Loader'
 
 describe('Test series 1', () => {
   it('Loader description', () => {
