91ee1ca4821254f61603499642e46690343ac9ea
Nello Brunelli <nello.brunelli@wyscout.com>
2023-04-01 11:39:34 +0200

    test loader with query

--- src/components/Loader.test.tsx
+++ src/components/Loader.test.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react'
 
 import Loader from './Loader'
 
-describe('ecco un test', () => {
-  it('il mio primo test ', () => {
-    const loader = render(<Loader />)
-    console.log(loader);
-    
-    expect(1).toBe(1)
+describe('Test series 1', () => {
+  it('Loader description', () => {
+    render(<Loader />)
+    const el = screen.getByText(/loading dogs/i)
+    expect(el).toBeInTheDocument()
   })
 })
